7534507842ee719cb05c29db7933ac8d135fda6e
Sondre Stokkan Spæren <89384544+Sondre-S@users.noreply.github.com>
2024-05-21 09:58:16 +0200

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Sondre Stokkan Spæren on 21/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
